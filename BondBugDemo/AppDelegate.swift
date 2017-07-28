85696565229f87f0f851cb8ad1969aa0281fe39f
Ian Kazlauskas <nayzak90@gmail.com>
2017-07-28 10:50:15 +0900

    demo

---
+++ BondBugDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BondFix
+//
+//  Created by Ian Kazlauskas on 28/07/2017.
+//  Copyright © 2017 Ian Kazlauskas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ BondBugDemo/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  BondFix
+//
+//  Created by Ian Kazlauskas on 28/07/2017.
+//  Copyright © 2017 Ian Kazlauskas. All rights reserved.
+//
+
+import Cocoa
+import ReactiveKit
+import Bond
+
+class ViewController: NSViewController {
+
+  struct ViewModel {
+
+    let title: String
+
+  }
+  
+  @IBOutlet weak var tableView: NSTableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let viewModels: SafeSignal<[ViewModel]> = .just([
+      ViewModel(title: "One"),
+      ViewModel(title: "Two"),
+      ViewModel(title: "Three")
+    ])
+
+    viewModels.bind(to: tableView, animated: true) { titles, row, tableView in
+      let cell = tableView.make(withIdentifier: "CellID", owner: nil) as! NSTableCellView
+      cell.textField?.stringValue = titles[row].title
+      return cell
+    }
+  }
+
+}
+
